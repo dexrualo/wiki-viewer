a183c0fe8b1bcfe9d0f2917b3e1cf9c37e8fd005
Dexter Rualo <dexterrualo@gmail.com>
2017-01-17 21:21:34 +0800

    Removed js code for page content

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   var title = "";
-  var resultDIV = {};
   $(".search-button").click(function(){
     var searchText = $(".search-box").val();
     var uri = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch="+ searchText +"&srprop=snippet&callback=?"
@@ -9,11 +8,7 @@ $(document).ready(function() {
       json.query.search.forEach(function(value,index,array){
         title = value.title
         $("<div class='result-div'></div>").appendTo(".results").click(function(){
-          var uri = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page="+ $(this).children(".title").text() +"&prop=text&callback=?";
-          $.getJSON(uri, function(json){
-            console.log(json.parse.text["*"]);
-            $(".page").html(json.parse.text["*"]);
-          });
+          
         });
         $("<p class='title'>"+value.title+"</p>").appendTo(".result-div:last");
         $("<p>"+value.snippet+"...</p>").appendTo(".result-div:last");

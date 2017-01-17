86ecc3f8c6a2bc8b0b75c81c4445400d2d956e70
Dexter Rualo <dexterrualo@gmail.com>
2017-01-17 13:50:22 +0800

    Added page view

--- js/scripts.js
+++ js/scripts.js
@@ -3,33 +3,21 @@ $(document).ready(function() {
   var resultDIV = {};
   $(".search-button").click(function(){
     var searchText = $(".search-box").val();
-    console.log(searchText);
     var uri = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch="+ searchText +"&srprop=snippet&callback=?"
       
-    function openArticle(){
-      var uri = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page="+ $(this>".title").text() +"&prop=text&callback=?"
-      $.getJSON(uri, function(json){
-        alert(uri);
-      });
-    }
-
     $.getJSON(uri, function(json){
       json.query.search.forEach(function(value,index,array){
-        console.log(value);
         title = value.title
         $("<div class='result-div'></div>").appendTo(".results").click(function(){
           var uri = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page="+ $(this).children(".title").text() +"&prop=text&callback=?";
           $.getJSON(uri, function(json){
-            alert(uri);
+            console.log(json.parse.text["*"]);
+            $(".page").html(json.parse.text["*"]);
           });
         });
         $("<p class='title'>"+value.title+"</p>").appendTo(".result-div:last");
         $("<p>"+value.snippet+"...</p>").appendTo(".result-div:last");
       });
     });
-    
   });
-
-  
 });
-

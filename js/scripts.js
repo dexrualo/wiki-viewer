3cb25c83044754b76eeeb381f3219ad4d494b9c3
Dexter Rualo <dexterrualo@gmail.com>
2017-01-17 09:49:24 +0800

    Added api call for content. Still not running.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
+  var title = "";
+  var resultDIV = {};
   $(".search-button").click(function(){
     var searchText = $(".search-box").val();
     console.log(searchText);
-    var uri = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch="+ searchText +"&srprop=snippet"
-    $.getJSON(uri, function(json){ json.query.search.forEach(function(value,index,array){
+    var uri = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch="+ searchText +"&srprop=snippet&callback=?"
+      
+    function openArticle(){
+      var uri = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page="+ $(this>".title").text() +"&prop=text&callback=?"
+      $.getJSON(uri, function(json){
+        alert(uri);
+      });
+    }
+
+    $.getJSON(uri, function(json){
+      json.query.search.forEach(function(value,index,array){
         console.log(value);
-        $("<div class='result-div'></div>").appendTo(".container");
-        $("<p>"+value.title+"</p>").appendTo(".result-div:last");
+        title = value.title
+        $("<div class='result-div' onclick='openArticle();'></div>").appendTo(".results");
+        $("<p class='title'>"+value.title+"</p>").appendTo(".result-div:last");
         $("<p>"+value.snippet+"...</p>").appendTo(".result-div:last");
       });
     });
+    
   });
+
+  
 });
+

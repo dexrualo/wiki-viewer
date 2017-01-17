de0625182a55f37228a9e877f17af804712d733d
Dexter Rualo <dexterrualo@gmail.com>
2017-01-17 10:05:02 +0800

    Fixed selected search result api call uri.

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,12 @@ $(document).ready(function() {
       json.query.search.forEach(function(value,index,array){
         console.log(value);
         title = value.title
-        $("<div class='result-div' onclick='openArticle();'></div>").appendTo(".results");
+        $("<div class='result-div'></div>").appendTo(".results").click(function(){
+          var uri = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page="+ $(this).children(".title").text() +"&prop=text&callback=?";
+          $.getJSON(uri, function(json){
+            alert(uri);
+          });
+        });
         $("<p class='title'>"+value.title+"</p>").appendTo(".result-div:last");
         $("<p>"+value.snippet+"...</p>").appendTo(".result-div:last");
       });

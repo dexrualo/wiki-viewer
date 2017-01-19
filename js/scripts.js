43a6da3e1c03def964e4e07ce85e491d90837a7a
Dexter Rualo <dexterrualo@gmail.com>
2017-01-19 14:30:47 +0800

    Cleaned up functionality and added show and hide effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,35 @@
 $(document).ready(function() {
   $(".search-button").click(function(){
-
+    if ($(".search-box").val().length == 0)
+      return;
     var searchText = $(".search-box").val();
-    
-    $(".results").html("");
+    if ($(".results").css("display") == "block")
+      $(".results").hide("fast");
+    if ($(".page").css("display") == "block")
+      $(".page").hide("fast");
+    $(".results").html("").show("fast");
+
     $.getJSON("https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&exsentences=1&exlimit=max&gsrsearch=" + searchText + "&callback=?", function(json){
       var data = {};
       for (page in json.query.pages){
         data = json.query.pages[page];
         $("<div class='result-div' data-pageid='"+ data.pageid +"'></div>").appendTo(".results").click(function(){
           var pageid = this.dataset.pageid;
-          //window.open("https://en.wikipedia.org/?curid=" + this.dataset.pageid);
           $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&pageids=" + this.dataset.pageid + "&callback=?", function(json){
-            $(".results").hide();
-            $(".page").html("<h1>"+json.query.pages[pageid].title+"</h1>" + json.query.pages[pageid].extract);
+            $(".results").hide("fast", function(){
+              $(".page").html("<h1>"+json.query.pages[pageid].title+"</h1>" + json.query.pages[pageid].extract).show("fast");
+              $("<button class='back-button'>&lt;&lt;Back</button>").insertBefore("h1").click(function(e){
+                e.preventDefault();
+                $(".page").hide("fast", function(){
+                  $(".results").show("fast");
+                });
+              });
+            });
           });
         });
-        //$("<p class='title'>" + data.title + "</p>").appendTo(".result-div:last");
-        $("<p>" + data.extract + "</p>").appendTo(".result-div:last");
+        if (data.thumbnail)
+          $("<img class='thumbnail' src='" + data.thumbnail.source + "'/>").appendTo(".result-div:last");
+        $("<div>" + data.extract + "</div>").appendTo(".result-div:last");
       };
     });
   });
@@ -25,7 +37,6 @@ $(document).ready(function() {
     if (e.which == 13) {
       e.preventDefault();
       $(".search-button").click();
-      $(".results").show();
     }
   });
 });
